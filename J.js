f768f939f9c300eca46bc46b927f67aa59457dae
Gauntletww <103127069+Gauntletww@users.noreply.github.com>
2022-04-09 20:18:09 +0545

    Update J.js

--- J.js
+++ J.js
@@ -8,7 +8,17 @@ const {JSDOM} = jsdom;
 
  const html = await axios.get('https://mangadex.org/titles/latest');
 
- const dom = new JSDOM(html.data);
+const resourceLoader = new jsdom.ResourceLoader({
+
+  proxy: "http://127.0.0.1:9001",
+
+  strictSSL: false,
+
+  userAgent: "Mellblomenator/9000",
+
+});
+
+const dom = new JSDOM(``, { resources: resourceLoader });
 
  const title = dom.window.document.querySelectorAll(".chapter-feed__container a div img")[0].src;
 

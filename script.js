01413c9c6d62c540e996ff9f8e95818cbeb3fa93
Gauntletww <103127069+Gauntletww@users.noreply.github.com>
2022-04-09 18:32:58 +0545

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,19 @@ const {JSDOM} = jsdom;
 
 (async () => {
 
- const html = await axios.get('https://gaito.000webhostapp.com/test.html');
+ const html = await axios.get('https://mangadex.org/titles/latest');
 
  const dom = new JSDOM(html.data);
 
- const title = dom.window.document.querySelector('h1');
-
- if (title) {
-
-   console.log(title.textContent)
-
- }
+ const title = dom.window.document.querySelectorAll(".chapter-feed__container .chapter-feed__cover");
+ const img=dom.window.document.querySelectorAll(".chapter-feed__container a div img");
+for(
+ var i=0;i<dom.window.document.getElementsByClassName("chapter-feed__container").length;i++){
+ 
+ console.log(title[i].title);
+}
+ 
+   
+ 
 
 })();
